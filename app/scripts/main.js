8b9723235b7a2e83ba89abf2ebe3c9f04e151830
Scott Hyndman <shyndman@google.com>
2015-03-16 13:24:36 -0400

    New deploy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,21 +3,24 @@
   frame.addEventListener('click', function() {
     frame.style.boxShadow = 'none';
     var anim = new Animation(frame, test, {
-      easing: 'cubic-bezier(0.4, 0.0, 0.2, 1)',
-      duration: 600
+      easing: 'cubic-bezier(0.4, 0.0, 1, 1)',
+      duration: 500
     });
     document.timeline.play(anim);
   });
 
-  var frameFrom = 0.5, frameTo = 20,
+  var frameFrom = 0.5, frameTo = 16,
+      imgFrom = 0, imgTo = 0,
       img = document.querySelector('img');
 
   function test(t, ele, anim) {
     if (t == null) t = 1;
 
     var frameScale = t * (frameTo - frameFrom) + frameFrom;
+    var imgY = t * (imgTo - imgFrom) + imgFrom;
 
-    ele.style.transform = 'scale(' + frameScale + ')';
-    img.style.transform = 'scale(' + (1 / frameScale) + ')';
+    ele.style.transform = ele.style.webkitTransform = 'scale(' + frameScale + ') translateZ(0)';
+    img.style.transform = img.style.webkitTransform =
+      'scale(' + (1 / frameScale) + ') translateY(' + imgY + 'px) translateX(' + imgY + 'px) translateZ(0)';
   }
 }(window);
