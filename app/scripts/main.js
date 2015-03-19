68722d0f51872a6cd8c22ceb7cb96b5649103ec2
Scott Hyndman <shyndman@google.com>
2015-03-19 15:23:15 -0400

    Got translation locking

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,13 @@
 !function(global) {
   var borderRegions,
+      container = document.querySelector('#container'),
       frame = document.querySelector('#frame'),
       frameBounds = frame.getBoundingClientRect(),
+      background = document.querySelector('.background'),
       shadowLayer = document.querySelector('#shadows'),
       canvas = document.querySelector('canvas'),
-      ctx = canvas.getContext('2d');
+      ctx = canvas.getContext('2d'),
+      backgroundCheck = document.querySelector('#has-background');
 
   global.goog.shadowPatches.generate().then(function(patches) {
     canvas.style.opacity = 1;
@@ -13,14 +16,14 @@
 
   var p1, isMouseDown = false;
 
-  document.body.onmousedown = function(e) {
+  container.onmousedown = function(e) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     canvas.classList.remove('fade-out');
     isMouseDown = true;
     p1 = getEventPoint(e);
   };
 
-  document.body.onmousemove = function(e) {
+  container.onmousemove = function(e) {
     if (!isMouseDown) {
       return;
     }
@@ -37,7 +40,7 @@
     ctx.restore();
   };
 
-  document.body.onmouseup = function(e) {
+  container.onmouseup = function(e) {
     canvas.classList.add('fade-out');
     isMouseDown = false;
     run(extractRect(p1, getEventPoint(e)));
@@ -48,21 +51,36 @@
     return { x: e.pageX - bounds.left, y: e.pageY - bounds.top };
   }
 
+  backgroundCheck.onchange = function() {
+    background.style.display = backgroundCheck.checked ? '' : 'none';
+  };
+  backgroundCheck.onchange();
+
   var anim, player;
 
   function run(rect) {
     shadowLayer.innerHTML = '';
+    background.style.transform = '';
     frame.style.boxShadow = 'none';
 
     var scaleX = rect.width / frameBounds.width,
         scaleY = rect.height / frameBounds.height
 
-    anim = new global.goog.TransformAnimationNode(frame)
+    var node =
+      new global.goog.TransformAnimationNode(frame)
         .translate(rect.left, rect.top)
         .scale(scaleX, scaleY)
         .transition(500, 'cubic-bezier(0.4, 0.0, 1, 1)')
         .withBorderRegions(borderRegions, shadowLayer)
-        .build();
+
+    if (backgroundCheck.checked) {
+      node.addChild(
+        new global.goog.TransformAnimationNode(background)
+          .scaleLocked(true)
+          .translationLocked(true))
+    }
+
+    anim = node.build();
     player = document.timeline.play(anim);
   }
 
